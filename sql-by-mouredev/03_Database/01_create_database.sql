8414ece7589ebbb6a96726d5cb002763d803f7b9
lgandarillas <lgandaf@gmail.com>
2024-02-25 16:47:13 +0100

    add: 03_Database

---
+++ sql-by-mouredev/03_Database/01_create_database.sql
@@ -0,0 +1,9 @@
+/***
+hello-sql
+By: @mouredev
+
+01_create_database.sql
+***/
+
+-- Crea una base de datos llamada 'tabla'
+CREATE DATABASE tabla;
---
+++ sql-by-mouredev/03_Database/02_drop_database.sql
@@ -0,0 +1,9 @@
+/***
+hello-sql
+By: @mouredev
+
+02_drop_database.sql
+***/
+
+-- Elimina la base de datos llamada "test"
+DROP DATABASE test;
